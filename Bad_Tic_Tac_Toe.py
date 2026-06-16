1cec1cbbf1506b6ff10486f3f8f42a44a982052c
Gavin Binkley <gavingsb@me.com>
2026-06-16 16:46:18 -0400

    Created the bad code file and added the functions

---
+++ Bad_Tic_Tac_Toe.py
@@ -0,0 +1,20 @@
+#This is the bad code version of a tic-tac-toe program
+
+print("welcome to Tic Tac Toe")
+print("This program may later include online multiplayer, score tracking, colors, AI, and Tournaments")
+print("None of these features are actually included right now.")
+
+def show_board():
+
+
+def player_x_turn()
+
+
+def player_o_turn()
+
+
+def check_stuff()
+
+
+def useless_future_features()
+
