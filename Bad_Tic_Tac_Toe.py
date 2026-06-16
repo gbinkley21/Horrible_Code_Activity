c839c69516fedb9e61cc910bfe325b2becce2cc0
Gavin Binkley <gavingsb@me.com>
2026-06-16 17:29:57 -0400

    Bad_Tic_Tac_Toe.py didn't actually run so I fixed the "bad code" so that it runs but it doesnt look good

--- Bad_Tic_Tac_Toe.py
+++ Bad_Tic_Tac_Toe.py
@@ -1,62 +1,72 @@
 #This is the bad code version of a tic-tac-toe program
-
+#Author: Gavin Binkley
+#Date: 6/16/26
 print("welcome to Tic Tac Toe")
 print("This program may later include online multiplayer, score tracking, colors, AI, and Tournaments")
 print("None of these features are actually included right now.")
 
 def show_board():
     global a, b, c, d, e, f, g, h, i
+    print()
     print(a + " | " + b + " | " + c)
     print("--+---+--")
     print(d + " | " + e + " | " + f)
     print("--+---+--")
     print(g + " | " + h + " | " + i)
+    print()
 
 def player_x_turn():
     print("Player X Turn")
+    global a, b, c, d, e, f, g, h, i
     spot = input("Player X, choose a spot 1-9: ")
-    if spot == '1':
-        a = 'X'
-    if spot == '2':
-        b = 'X'
-    if spot == '3':
-        c = 'X'
-    if spot == '4':
-        d = 'X'
-    if spot == '5':
-        e = 'X'
-    if spot == '6':
-        f = 'X'
-    if spot == '7':
-        g = 'X'
-    if spot == '8':
-        h = 'X'
-    if spot == '9':
-        i = 'X'
+    if spot == "1" and a == "1":
+        a = "X"
+    elif spot == "2" and b == "2":
+        b = "X"
+    elif spot == "3" and c == "3":
+        c = "X"
+    elif spot == "4" and d == "4":
+        d = "X"
+    elif spot == "5" and e == "5":
+        e = "X"
+    elif spot == "6" and f == "6":
+        f = "X"
+    elif spot == "7" and g == "7":
+        g = "X"
+    elif spot == "8" and h == "8":
+        h = "X"
+    elif spot == "9" and i == "9":
+        i = "X"
+    else:
+        print("That spot is taken or invalid. You lose your turn.")
 
 
 
 def player_o_turn():
-    print("Player Y Turn")
-    spot = input("Player Y, choose a spot 1-9: ")
-    if spot == '1':
-        a = ('O')
-    if spot == '2':
-        b = 'O'
-    if spot == '3':
-        c = 'O'
-    if spot == '4':
-        d = 'O'
-    if spot == '5':
-        e = 'O'
-    if spot == '6':
-        f = 'O'
-    if spot == '7':
-        g = 'O'
-    if spot == '8':
-        h = 'O'
-    if spot == '9':
-        i = 'O'
+    print("Player O Turn")
+    global a, b, c, d, e, f, g, h, i
+    spot = input("Player O, choose a spot 1-9: ")
+
+    if spot == "1" and a == "1":
+        a = "O"
+    elif spot == "2" and b == "2":
+        b = "O"
+    elif spot == "3" and c == "3":
+        c = "O"
+    elif spot == "4" and d == "4":
+        d = "O"
+    elif spot == "5" and e == "5":
+        e = "O"
+    elif spot == "6" and f == "6":
+        f = "O"
+    elif spot == "7" and g == "7":
+        g = "O"
+    elif spot == "8" and h == "8":
+        h = "O"
+    elif spot == "9" and i == "9":
+        i = "O"
+    else:
+        print("That spot is taken or invalid. You lose your turn.")
 
 
 def check_stuff():
